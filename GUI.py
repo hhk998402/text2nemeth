c24f9a8cb078b643649ce4ca6b07219f140f9d9e
hhk998402 <hemant.hari.kumar@gmail.com>
2017-10-26 00:49:14 +0530

    Text to Nemeth - With Changes

---
+++ GUI.py
@@ -0,0 +1,35 @@
+"""
+Created on Wed Oct 11 00:29:21 2017
+
+@author: hemanthkumar
+"""
+
+import pybrl as brl
+import Tkinter
+from Tkinter import *
+
+master = Tk()
+
+e = Entry(master)
+e.pack()
+
+e.focus_set()
+
+def callback():
+    example=brl.translate(e.get())
+    #print(example)
+    T.delete("1.0", Tkinter.END)
+    T.insert(END,brl.toUnicodeSymbols(example, flatten=True))
+    #print e.get()
+
+b = Button(master, text="Convert to Braille", width=20, command=callback)
+b.pack()
+T = Text(master, height=2, width=30)
+T.pack()
+
+mainloop()
+e = Entry(master, width=1000)
+e.pack()
+
+
+text = e.get()
